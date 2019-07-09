8e8da8c54b1408cabdd1cdce8094489685d0a848
李师师 <liss39@midea.com>
2019-07-09 14:57:56 +0800

    first commit

---
+++ pages/user/_id.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>这是用户{{$route.params.id}}页面</h1>
+</template>
+<script>
+export default {
+    transition:'test'
+}
+</script>
+
---
+++ pages/user/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>这是id默认页面</h1>
+</template>
+<script>
+export default {
+    transition:'test'
+}
+</script>
+<style lang="less" scoped>
+
+</style>
